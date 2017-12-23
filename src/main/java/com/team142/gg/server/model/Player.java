f036341efc2749d662ed074b0417e189d88583a9
Justin Tamblyn <just1689@gmail.com>
2017-12-23 13:27:50 +0200

    Added basic scoreboard to player

--- src/main/java/com/team142/gg/server/model/Player.java
+++ src/main/java/com/team142/gg/server/model/Player.java
@@ -5,6 +5,7 @@
  */
 package com.team142.gg.server.model;
 
+import java.util.Map;
 import lombok.Data;
 
 /**
@@ -17,10 +18,25 @@ public class Player {
     private final String id;
     private String name;
     private final long joinTimeMs;
+    private int score = 0;
 
     public Player(String id) {
         this.id = id;
         this.joinTimeMs = System.currentTimeMillis();
+    }
+
+    public void addKill() {
+        score++;
+    }
+
+    public void addDeath() {
+        if (score > 0) {
+            score--;
+        }
+    }
+
+    public void addScoreToBoard(Map<String, Integer> map) {
+        map.put(name, score);
     }
 
 }
