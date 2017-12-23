1a4c4ebf0a95b5c64a2886422c98db5046ee4b02
Justin Tamblyn <just1689@gmail.com>
2017-12-23 13:28:19 +0200

    Simpler and lighter implementation of game summary

--- src/main/java/com/team142/gg/server/model/messages/MessageGameSummary.java
+++ src/main/java/com/team142/gg/server/model/messages/MessageGameSummary.java
@@ -5,28 +5,19 @@
  */
 package com.team142.gg.server.model.messages;
 
-import com.team142.gg.server.model.Game;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Value;
 
 /**
  *
  * @author just1689
  */
-@Data
-@NoArgsConstructor
+@Value
 @AllArgsConstructor
 public class MessageGameSummary {
 
     private String id;
     private String name;
     private int numberPlaying;
-
-    public MessageGameSummary(Game game) {
-        this.id = game.getId();
-        this.name = game.getName();
-        this.numberPlaying = 1;
-    }
 
 }
