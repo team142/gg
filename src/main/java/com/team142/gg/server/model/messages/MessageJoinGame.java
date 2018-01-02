7d4bed5367c6c0342af818d8e46d971dd44e20d5
Justin Tamblyn <just1689@gmail.com>
2018-01-02 14:00:36 +0200

    Handle users join game

--- src/main/java/com/team142/gg/server/model/messages/MessageJoinGame.java
+++ src/main/java/com/team142/gg/server/model/messages/MessageJoinGame.java
@@ -5,6 +5,8 @@
  */
 package com.team142.gg.server.model.messages;
 
+import com.team142.gg.server.controller.Referee;
+import com.team142.gg.server.model.messages.base.Message;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,8 +16,14 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor
-public class MessageJoinGame {
+public class MessageJoinGame extends Message implements Runnable {
 
     private String id;
+
+    @Override
+    public void run() {
+        Referee.handle(getFrom(), this);
+
+    }
 
 }
