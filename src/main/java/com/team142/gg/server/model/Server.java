b7ec8777ed8bf1963c5aead1053ee7afe10f368e
Captain Justin <just1689@gmail.com>
2018-01-03 22:25:16 +0200

    Share tag on connect to server

--- src/main/java/com/team142/gg/server/model/Server.java
+++ src/main/java/com/team142/gg/server/model/Server.java
@@ -5,8 +5,10 @@
  */
 package com.team142.gg.server.model;
 
+import com.team142.gg.server.controller.PostOffice;
 import com.team142.gg.server.controller.map.MapMaker;
 import com.team142.gg.server.controller.map.MapSettings;
+import com.team142.gg.server.model.messages.outgoing.other.MessageShareTag;
 import java.math.BigDecimal;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -19,25 +21,25 @@ import javax.websocket.Session;
  * @author just1689
  */
 public class Server {
-
+    
     public static final ConcurrentHashMap<String, Player> PLAYERS_ON_SERVER = new ConcurrentHashMap<String, Player>();
     public static final ConcurrentHashMap<String, Game> GAMES_ON_SERVER = new ConcurrentHashMap<String, Game>();
     public static final ConcurrentHashMap<String, Session> SESSIONS_ON_SERVER = new ConcurrentHashMap<String, Session>();
     public static final int TICK_MS = 80;
     public static final BigDecimal DEFAULT_SPEED = new BigDecimal(0.125);
     public static final AtomicInteger TAGS = new AtomicInteger(1000);
-
+    
     static {
         createDefaultGame();
-
+        
     }
-
+    
     private static void createDefaultGame() {
         Logger.getLogger(Server.class.getName()).log(Level.INFO, "Creating the default game");
         createGame("Default game", new MapSettings("meh"));
-
+        
     }
-
+    
     public static Game createGame(String name, MapSettings settings) {
         Logger.getLogger(Server.class.getName()).log(Level.INFO, "Creating a new game");
         Game game = new Game(name);
@@ -45,9 +47,9 @@ public class Server {
         GAMES_ON_SERVER.put(game.getId(), game);
         game.start();
         return game;
-
+        
     }
-
+    
     public static void playerDisconnects(String id) {
         Server.PLAYERS_ON_SERVER.remove(id);
         GAMES_ON_SERVER.values().stream()
@@ -55,25 +57,27 @@ public class Server {
                 .findFirst()
                 .ifPresent((game) -> game.removePlayer(id));
         SESSIONS_ON_SERVER.remove(id);
-
+        
     }
-
+    
     public static Game getGameByPlayer(String id) {
         return GAMES_ON_SERVER.values().stream()
                 .filter(e -> e.hasPlayer(id))
                 .findFirst()
                 .orElse(null);
     }
-
+    
     public static void newPlayer(Player player) {
         PLAYERS_ON_SERVER.put(player.getId(), player);
+        PostOffice.sendPlayerAMessage(player.getId(), new MessageShareTag(player.getTAG()));
+        
     }
-
+    
     public Game getGameByPlayer(Player player) {
         if (player == null) {
             return null;
         }
         return getGameByPlayer(player.getId());
     }
-
+    
 }
