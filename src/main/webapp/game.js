c8f272121b7ca1ed02dfbc19c103f460b3e183e3
Justin Tamblyn <just1689@gmail.com>
2018-01-03 09:33:32 +0200

    Enabled offline caching

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -106,6 +106,7 @@ var createScene = function () {
 
     // This creates a basic Babylon Scene object (non-mesh)
     var scene = new BABYLON.Scene(engine);
+    scene.name = "scene";
 
     // This creates and positions a free camera (non-mesh)
     var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
