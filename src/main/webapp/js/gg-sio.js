c3a543745a30758e7f8d83d05598a50f176dafe1
Justin Tamblyn <just1689@gmail.com>
2018-01-08 08:08:46 +0200

    Refactoring

--- src/main/webapp/js/gg-sio.js
+++ src/main/webapp/js/gg-sio.js
@@ -25,6 +25,73 @@ sio.sendKeyDown = function (key) {
     sio.send(JSON.stringify(message))
 }
 
+sio.postBox = function (event) {
+
+
+    var obj = JSON.parse(event.data)
+    var conversation = obj.conversation
+    if (conversation == "S_CHANGE_VIEW") {
+        web.changeView(obj.view)
+    } else if (conversation == "S_LIST_OF_GAMES") {
+        web.showListOfGames(obj.games)
+    } else if (conversation == "S_SCOREBOARD") {
+        obj.TAGS.forEach(t => {
+            if (getPlayerByTag(t)) {
+                baby.createSphereIfNotExists(t)
+            }
+        })
+    } else if (conversation == "S_SHARE_TAG") {
+        tag = obj.tag
+
+    } else if (conversation == "S_SHARE_MAP") {
+        baby.createMap(obj.MAP)
+
+    } else if (conversation == "S_PLAY_SOUND") {
+        gSound.playSound(obj.FILE)
+
+    } else if (conversation == "S_PLAYER_LEFT") {
+
+        var tagToRemove = obj.tag
+        var l = playerTanks.length
+        var indexToRemove = -1
+        for (var i = 0; i < l; i++) {
+            var key = playerTanks[i].key
+            if (key == tagToRemove) {
+                indexToRemove = i
+                playerTanks[i].value.dispose()
+            }
+        }
+        if (indexToRemove > -1) {
+            playerTanks.splice(indexToRemove, 1)
+        }
+
+
+    } else if (conversation == "S_SHARE_DYNAMIC_THINGS") {
+        var l = obj.THINGS.length
+        for (var i = 0; i < l; i++) {
+            if (obj.THINGS[i].tag == tag) {
+                // if (camera) {
+                camera.position.x = obj.THINGS[i].x
+                camera.position.y = obj.THINGS[i].y + 0.25
+                camera.position.z = obj.THINGS[i].z
+                camera.rotation.y = obj.THINGS[i].rotation
+                // }
+            }
+            var s = getPlayerByTag(obj.THINGS[i].tag)
+            if (s) {
+                s.position.x = obj.THINGS[i].x
+                s.position.y = obj.THINGS[i].y
+                s.position.z = obj.THINGS[i].z
+                s.rotation.y = obj.THINGS[i].rotation
+
+            }
+        }
+
+    }
+    // alert("Unhandled message" + event.data)
+}
+
+
 sio.assignMethods = function () {
     socket.onopen = function (event) {
         var body = {
@@ -33,81 +100,12 @@ sio.assignMethods = function () {
         }
         var json = JSON.stringify(body)
         socket.send(json)
-
     }
 
-    socket.onmessage = function (event) {
+    socket.onmessage = sio.postBox;
 
-        var obj = JSON.parse(event.data)
-        var conversation = obj.conversation
-        if (conversation == "S_CHANGE_VIEW") {
-            web.changeView(obj.view)
-
-        } else if (conversation == "S_LIST_OF_GAMES") {
-            web.showListOfGames(obj.games)
-
-
-        } else if (conversation == "S_SCOREBOARD") {
-            //First check who is missing from client
-            var l = obj.TAGS.length
-            for (var i = 0 i < l i++) {
-                var item = getPlayerByTag(obj.TAGS[i])
-                if (!item) {
-                    baby.createSphereIfNotExists(obj.TAGS[i])
-                }
-            }
-
-
-        } else if (conversation == "S_SHARE_TAG") {
-            tag = obj.tag
-
-        } else if (conversation == "S_SHARE_MAP") {
-            baby.createMap(obj.MAP)
-
-        } else if (conversation == "S_PLAY_SOUND") {
-            gSound.playSound(obj.FILE)
-
-        } else if (conversation == "S_PLAYER_LEFT") {
-
-            var tagToRemove = obj.tag
-            var l = playerTanks.length
-            var indexToRemove = -1
-            for (var i = 0; i < l; i++) {
-                var key = playerTanks[i].key
-                if (key == tagToRemove) {
-                    indexToRemove = i
-                    playerTanks[i].value.dispose()
-                }
-            }
-            if (indexToRemove > -1) {
-                playerTanks.splice(indexToRemove, 1)
-            }
-
-
-        } else if (conversation == "S_SHARE_DYNAMIC_THINGS") {
-            var l = obj.THINGS.length
-            for (var i = 0; i < l; i++) {
-                if (obj.THINGS[i].tag == tag) {
-                    // if (camera) {
-                    camera.position.x = obj.THINGS[i].x
-                    camera.position.y = obj.THINGS[i].y + 0.25
-                    camera.position.z = obj.THINGS[i].z
-                    camera.rotation.y = obj.THINGS[i].rotation
-                    // }
-                }
-                var s = getPlayerByTag(obj.THINGS[i].tag)
-                if (s) {
-                    s.position.x = obj.THINGS[i].x
-                    s.position.y = obj.THINGS[i].y
-                    s.position.z = obj.THINGS[i].z
-                    s.rotation.y = obj.THINGS[i].rotation
-
-                }
-            }
-
-        }
-        // alert("Unhandled message" + event.data)
-    }
+    // socket.onmessage = function (event) {
+    // }
 }
 
 sio.send = function (msg) {
