79e117a5a00dba174026c5f343d6dccf4f404b62
Captain Justin <just1689@gmail.com>
2018-01-13 16:52:10 +0200

    Spray message

---
+++ src/main/java/com/team142/gg/server/model/messages/outgoing/rendered/MessageSpray.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team142.gg.server.model.messages.outgoing.rendered;
+
+import com.team142.gg.server.model.messages.base.ConversationType;
+import com.team142.gg.server.model.messages.base.Message;
+import lombok.Getter;
+
+/**
+ *
+ * @author just1689
+ */
+public class MessageSpray extends Message {
+
+    @Getter
+    private int tagId;
+
+    @Getter
+    private long ms;
+
+    public MessageSpray(int tagId, long ms) {
+        this.setConversation(ConversationType.S_SHARE_SPRAY.name());
+        this.tagId = tagId;
+        this.ms = ms;
+    }
+
+}
