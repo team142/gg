f5ce99f23205f8b48083cef5183a9fb81c625a5c
Justin Tamblyn <just1689@gmail.com>
2018-01-19 17:04:19 +0200

    Message for server stats

---
+++ src/main/java/com/team142/gg/server/model/messages/outgoing/stats/MessagePlayerJoinStats.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team142.gg.server.model.messages.outgoing.stats;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+/**
+ *
+ * @author just1689
+ */
+@AllArgsConstructor
+@Value
+public class MessagePlayerJoinStats {
+
+    private String server;
+
+}
