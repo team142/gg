53596ff1532bb04d9ac253d11b38d6963efc85c0
Justin Tamblyn <just1689@gmail.com>
2018-01-19 22:58:46 +0200

    keep a bitmap of the map in the game

---
+++ src/main/java/com/team142/gg/server/controller/map/TileBitmap.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team142.gg.server.controller.map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ *
+ * @author just1689
+ */
+@AllArgsConstructor
+@Data
+public class TileBitmap {
+
+    private boolean canMoveOver;
+    private boolean canShootOver;
+
+}
