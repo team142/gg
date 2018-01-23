a0ec60b93c9f7baa21ff70778c36ce2f6611fd08
Justin Tamblyn <just1689@gmail.com>
2018-01-23 07:02:33 +0200

    Refactored map into model  + controller

--- src/main/java/com/team142/gg/server/model/Game.java
+++ src/main/java/com/team142/gg/server/model/Game.java
@@ -8,13 +8,9 @@ package com.team142.gg.server.model;
 import com.team142.gg.server.controller.MessageManager;
 import com.team142.gg.server.controller.GameManager;
 import com.team142.gg.server.controller.SoundManager;
-import com.team142.gg.server.model.mappable.organic.TileBitmap;
-import com.team142.gg.server.model.mappable.organic.MapTileElement;
 import com.team142.gg.server.model.mappable.artificial.Tank;
 import com.team142.gg.server.model.messages.outgoing.other.MessageGameSummary;
 import com.team142.gg.server.model.messages.outgoing.other.MessagePlayerLeft;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -33,21 +29,24 @@ import lombok.Data;
 public class Game {
 
     private final String id;
-    private final List<Player> players = new CopyOnWriteArrayList<>();
     private final String name;
+    private final Map map;
+
+    private final List<Player> players = new CopyOnWriteArrayList<>();
     private final ConcurrentHashMap<String, Tank> TANKS = new ConcurrentHashMap<>();
-    private final List<MapTileElement> MAP;
-    private TileBitmap[][] bitmap;
+
     private SoundManager soundManager;
     private Thread pingThread;
 
-    private final double startHealth;
+    private double startHealth;
 
     public Game(String name) {
-        this.MAP = Collections.synchronizedList(new ArrayList<>());
         this.id = UUID.randomUUID().toString();
-        this.soundManager = new SoundManager(this.id);
         this.name = name;
+        this.map = new Map();
+
+        this.soundManager = new SoundManager(this.id);
+
         this.startHealth = 100;
         startPinger();
 
