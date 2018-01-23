4586e615104f2d385af4ee72a4f53af8d72a351e
Justin Tamblyn <just1689@gmail.com>
2018-01-23 09:25:08 +0200

    Back to JSON until I figure out WS + BBuffer

--- src/main/java/com/team142/gg/server/controller/MessageManager.java
+++ src/main/java/com/team142/gg/server/controller/MessageManager.java
@@ -10,7 +10,6 @@ import com.team142.gg.server.model.Repository;
 import com.team142.gg.server.model.messages.base.Message;
 import com.team142.gg.server.model.messages.base.ConversationMap;
 import com.team142.gg.server.utils.JsonUtils;
-import java.nio.ByteBuffer;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -54,34 +53,17 @@ public class MessageManager {
     }
 
     public static void sendPlayersAMessage(Game game, Message message) {
-        ByteBuffer bytes = JsonUtils.toByteBuffer(message);
-        game.getPlayers().forEach((player) -> sendPlayerAMessage(Repository.SESSIONS_ON_SERVER.get(player.getId()), bytes));
+        String json = JsonUtils.toJson(message);
+        game.getPlayers().forEach((player) -> sendPlayerAMessage(Repository.SESSIONS_ON_SERVER.get(player.getId()), json));
     }
 
     public static void sendPlayerAMessage(String playerId, Message message) {
-        ByteBuffer bytes = JsonUtils.toByteBuffer(message);
+        String json = JsonUtils.toJson(message);
         Session session = Repository.SESSIONS_ON_SERVER.get(playerId);
-        sendPlayerAMessage(session, bytes);
+        sendPlayerAMessage(session, json);
 
     }
 
-    public static void sendPlayerAMessage(Session session, ByteBuffer byteBuffer) {
-        if (session != null) {
-            session.getAsyncRemote().sendBinary(byteBuffer);
-        }
-    }
-
-    /**
-     *
-     * @param session
-     * @param json
-     * @deprecated 
-     * WARNING: try to use *sendPlayerAMessage(Session session, ByteBuffer
-     * byteBuffer)* instead as the overhead of using Strings is really bad
-     * (String pool etc). This will be left in for now as it may be useful
-     * and we would like communications to happen in this class.
-     *
-     */
     public static void sendPlayerAMessage(Session session, String json) {
         if (session != null) {
             session.getAsyncRemote().sendText(json);
