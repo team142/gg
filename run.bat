d7a24a9121bf6c91354f64e756b8ecf2f9f7e8c0
Justin Tamblyn <just1689@gmail.com>
2018-01-29 18:09:59 +0200

    Removed expired tokens

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ docker rm gg
 docker rmi team142/gg:local
 mvn clean install
 docker build -t team142/gg:local .
-docker run --name gg --publish 8080:8080 --env REPORT_SERVER_STATS_AS=dev --env PUSHOVER_USER=u4xfyuftwprmf5zyazdrbx7cnhs2o1 --env PUSHOVER_TOKEN=aywmwjtuz5rpdopx6a66w3wjzibdon team142/gg:local
+docker run --name gg --publish 8080:8080 --env REPORT_SERVER_STATS_AS=dev team142/gg:local
 
  
