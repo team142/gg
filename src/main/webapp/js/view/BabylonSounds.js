555120514b818147254648281398425d975c1c2a
Justin Tamblyn <just1689@gmail.com>
2018-02-05 10:46:50 +0200

    Refactor

--- src/main/webapp/js/view/BabylonSounds.js
+++ src/main/webapp/js/view/BabylonSounds.js
@@ -1,34 +1,30 @@
 import { baby } from '../model/Baby.js'
 
+const sounds = [
+    "sounds/pew.mp3",
+    "sounds/ding.mp3",
+    "sounds/explode.mp3",
+    "sounds/shhha.mp3"
+]
 
 export class BabylonSounds {
     static loadSounds() {
-        const sounds = [
-            "sounds/pew.mp3",
-            "sounds/ding.mp3",
-            "sounds/explode.mp3",
-            "sounds/shhha.mp3"
-        ]
         for (const s of sounds) {
             BabylonSounds.loadSound(s)
         }
-
     }
 
     static loadSound(path) {
-        var sound = new BABYLON.Sound(path, path, baby.scene)
+        const sound = new BABYLON.Sound(path, path, baby.scene)
         baby.soundData.soundMap.set(path, sound)
-
     }
 
     static playSound(key) {
         baby.soundData.soundMap.get(key).play()
-
     }
 
     static playPew() {
         BabylonSounds.playSound("sounds/pew.mp3")
-
     }
 
 }
