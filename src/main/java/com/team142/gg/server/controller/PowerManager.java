25e027cc387f2d12f060ed14603141b7023d1f16
Captain Justin <just1689@gmail.com>
2018-02-05 18:12:02 +0200

    Clean up

--- src/main/java/com/team142/gg/server/controller/PowerManager.java
+++ src/main/java/com/team142/gg/server/controller/PowerManager.java
@@ -7,8 +7,6 @@ package com.team142.gg.server.controller;
 
 import com.team142.gg.server.model.Player;
 import com.team142.gg.server.controller.runnable.powers.Power;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -16,15 +14,11 @@ import java.util.logging.Logger;
  */
 public class PowerManager {
 
-    private static final Logger LOG = Logger.getLogger(PowerManager.class.getName());
-
     public static void handle(Player player, String key) {
         Power power = player.getPowers().get(key);
         if (power != null) {
             power.run();
         }
-        LOG.log(Level.INFO, "POWER TIME: {0}, {1}", new Object[]{player.getName(), key});
-
     }
 
 }
