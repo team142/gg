8eb48053e26590f20e0eed9b1d82652dbda917d3
Justin Tamblyn <just1689@gmail.com>
2018-02-06 07:05:29 +0200

    Updated

--- run.sh
+++ run.sh
@@ -7,9 +7,13 @@ docker rmi team142/gg:local
 cd ../gg-client
 ./build.sh
 cd ../gg
+mkdir src/main/webapp
 cp -rf ../gg-client/build/ src/main/webapp/
 cp -rf src/main/WEB-INF src/main/webapp
 
 mvn clean install
+
+rm -rf src/main/webapp
+
 docker build -t team142/gg:local .
 docker run --name gg --publish 8080:8080 --env REPORT_SERVER_STATS_AS=dev team142/gg:local
