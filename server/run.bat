928cfa4db6b107e9c91747d3b96d5c4ce32c540f
Captain Justin <just1689@gmail.com>
2018-02-06 16:50:59 +0200

    Copy app and remove once image is built

--- server/run.bat
+++ server/run.bat
@@ -1,11 +1,15 @@
 cd ../client
 call build.bat
 cd ../server
+ROBOCOPY ../client/build/ src/main/webapp /MIR
 docker stop gg
 docker rm gg
 docker rmi team142/gg:local
 mvn clean install
 docker build -t team142/gg:local .
+
+rmdir src\main\webapp /s /q
+
 docker run --name gg --publish 8080:8080 --env REPORT_SERVER_STATS_AS=dev team142/gg:local
 
  
