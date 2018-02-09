560cdb981ddd0cb1e1eb9ba3dab816b04958280d
Captain Justin <just1689@gmail.com>
2018-02-09 20:41:42 +0200

    DAS ORB

---
+++ server/src/main/java/com/team142/gg/server/model/Orb.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team142.gg.server.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author just1689
+ */
+public class Orb {
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private double x, z;
+
+    public void use() {
+        //Ummm
+    }
+
+}
