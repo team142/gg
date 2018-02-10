fa0336b4f65eae784bac1aeef233ca418465a9c0
Captain Justin <just1689@gmail.com>
2018-02-10 21:35:10 +0200

    Stubbed out power 9

---
+++ server/src/main/java/com/team142/gg/server/controller/runnable/powers/Power09Hop180.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team142.gg.server.controller.runnable.powers;
+
+import com.team142.gg.server.model.Player;
+
+/**
+ *
+ * @author just1689
+ */
+public class Power09Hop180 extends Power {
+
+    public Power09Hop180(Player player, long refreshTime) {
+        super(player, 0, refreshTime);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+}
