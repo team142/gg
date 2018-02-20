87340e6eea29ecb3b346fe505c3e63a90898bd55
Justin Tamblyn <just1689@gmail.com>
2018-02-20 23:48:55 +0200

    Drop bomb impl

--- server/src/main/java/com/team142/gg/server/controller/runnable/powers/Power05DropBomb.java
+++ server/src/main/java/com/team142/gg/server/controller/runnable/powers/Power05DropBomb.java
@@ -5,7 +5,12 @@
  */
 package com.team142.gg.server.controller.runnable.powers;
 
+import com.team142.gg.server.controller.GameManager;
+import com.team142.gg.server.controller.PowerManager;
+import com.team142.gg.server.model.Game;
 import com.team142.gg.server.model.Player;
+import com.team142.gg.server.model.Repository;
+import com.team142.gg.server.model.mappable.artificial.Bullet;
 
 /**
  *
@@ -13,7 +18,7 @@ import com.team142.gg.server.model.Player;
  */
 public class Power05DropBomb extends Power {
 
-    private static final long INITIAL_COOLDOWN = 5000;
+    private static final long INITIAL_COOLDOWN = 15000;
 
     public Power05DropBomb(Player player) {
         super(5, player, 0, INITIAL_COOLDOWN, 1);
@@ -21,7 +26,24 @@ public class Power05DropBomb extends Power {
 
     @Override
     public void execute() {
+        playerShoots();
 
     }
 
+    public void playerShoots() {
+        Game game = Repository.GAMES_ON_SERVER.get(getPlayer().getGameId());
+
+        //Change state
+        Bullet bullet = getPlayer().createBullet();
+        bullet.setSpeed(0);
+        bullet.setDamage(100);
+
+        //Communicate
+        GameManager.sendBullet(game, bullet);
+        game.getSoundManager().sendShoot();
+
+        PowerManager.sendCooldown(getPlayer().getId(), this, 5);
+
+    }
+    
 }
