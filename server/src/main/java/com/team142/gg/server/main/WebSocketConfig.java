9c7e29ba3a98dc91ee38da7ac973faf852dcd6f3
earv1 <emmanuelarvanitis@gmail.com>
2018-02-21 20:20:09 +0200

    Change websocket location

--- server/src/main/java/com/team142/gg/server/main/WebSocketConfig.java
+++ server/src/main/java/com/team142/gg/server/main/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(greetingHandler(), "/greeting");
+        registry.addHandler(greetingHandler(), "/websocket");
     }
 
     @Bean
