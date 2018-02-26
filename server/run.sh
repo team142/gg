3bbe9e2b83c87a84a0b2b0e68e28590da5947316
Justin Tamblyn <just1689@gmail.com>
2018-02-26 17:37:10 +0200

    Stop reporting dev environments

--- server/run.sh
+++ server/run.sh
@@ -16,4 +16,4 @@ mvn clean install
 rm -rf src/main/webapp
 
 docker build -t team142/gg:local .
-docker run --name gg --publish 8080:8080 --env REPORT_SERVER_STATS_AS=dev team142/gg:local
+docker run --name gg --publish 8080:8080 team142/gg:local
