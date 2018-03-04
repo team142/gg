3ce642a50808da3d4d1e028e3032a090b365ede1
earv1 <emmanuelarvanitis@gmail.com>
2018-03-04 15:19:39 +0200

    Now runs from jar

--- server/run.sh
+++ server/run.sh
@@ -17,7 +17,8 @@ mkdir -p ${frontendFolder}
 cp -rf ../client/build/* ${frontendFolder}
 cp -rf src/main/WEB-INF ${frontendFolder}
 
-gradle bootRun
+gradle bootRepackage
+java -jar ./build/libs/gs-messaging-stomp-websocket-0.1.0.jar
 
 
 #docker build -t team142/gg:local .
