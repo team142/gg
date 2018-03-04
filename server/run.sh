3804ea7f3dbc1a08fdebf2f400dca9598eb023f5
earv1 <emmanuelarvanitis@gmail.com>
2018-03-04 15:23:39 +0200

    Changing project name

--- server/run.sh
+++ server/run.sh
@@ -18,7 +18,7 @@ cp -rf ../client/build/* ${frontendFolder}
 cp -rf src/main/WEB-INF ${frontendFolder}
 
 gradle bootRepackage
-java -jar ./build/libs/gs-messaging-stomp-websocket-0.1.0.jar
+java -jar ./build/libs/gg-server-0.1.0.jar
 
 
 #docker build -t team142/gg:local .
