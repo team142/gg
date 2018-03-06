1761f63e6133c566085e92e711d1fc796c7a9060
Justin Tamblyn <just1689@gmail.com>
2018-03-06 14:54:32 +0200

    Time series events

---
+++ server/src/main/java/com/team142/gg/server/controller/runnable/TickerTimeseries.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team142.gg.server.controller.runnable;
+
+import com.team142.gg.server.model.EventRepository;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author just1689
+ */
+public class TickerTimeseries implements Runnable {
+
+    private static final long SLEEP_TIME_MS = 60000;
+
+    @Override
+    public void run() {
+        while (true) {
+            EventRepository.pruneAll();
+            try {
+                Thread.sleep(SLEEP_TIME_MS);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(TickerTimeseries.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+}
---
+++ server/src/main/java/com/team142/gg/server/model/EventRepository.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team142.gg.server.model;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author just1689
+ */
+public class EventRepository {
+
+    private static final ConcurrentHashMap<String, LinkedList<Long>> EVENTS = new ConcurrentHashMap<>();
+    private static final long EXPIRES_MS = 1 * 60 * 1000;
+
+    public static void pruneAll() {
+        EVENTS
+                .values()
+                .forEach(EventRepository::pruneList);
+    }
+
+    public static void pruneList(LinkedList<Long> list) {
+        Iterator<Long> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            pruneElement(iterator);
+        }
+    }
+
+    public static void pruneElement(Iterator<Long> iterator) {
+        long rightNow = System.currentTimeMillis();
+        if (rightNow - EXPIRES_MS > iterator.next()) {
+            iterator.remove();
+        }
+
+    }
+
+    public static synchronized int pushAndTell(String key, long interest) {
+        LinkedList<Long> list = EVENTS.get(key);
+        if (list == null) {
+            list = new LinkedList<>();
+            EVENTS.put(key, list);
+        }
+        list.add(System.currentTimeMillis());
+        int result = 0;
+        Iterator<Long> iterator = list.iterator();
+        long rightNow = System.currentTimeMillis();
+        while (iterator.hasNext()) {
+            if (rightNow - interest <= iterator.next()) {
+                result++;
+            }
+        }
+        return result;
+
+    }
+
+}
