9ab0e1bd374777974189e92b1f0a33d11baac204
Justin Tamblyn <just1689@gmail.com>
2018-03-10 15:23:18 +0200

    CORS

--- server/src/main/java/com/team142/gg/server/main/WebSocketConfig.java
+++ server/src/main/java/com/team142/gg/server/main/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(greetingHandler(), "/websocket");
+        registry.addHandler(greetingHandler(), "/websocket").setAllowedOrigins("*");
     }
 
     @Bean
