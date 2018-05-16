6e3747095be734c7503e52b06b2bf0ffda831518
Justin Tamblyn <just1689@gmail.com>
2018-05-16 13:19:33 +0200

    Changed bullets to use paths

--- client/js/model/Bullet.js
+++ client/js/model/Bullet.js
@@ -3,15 +3,46 @@ import {baby} from '../model/Baby.js'
 
 export class Bullet {
 
-    setupTicker() {
-        this.bulletTimer = setInterval(() => {
-            this.tick()
-        }, (17 * 3))
-
-    }
 
     static createAndSave(obj) {
-        baby.bullets.push(new Bullet(obj.BULLET, baby.baseBullet.clone("bullet" + BabylonUtils.getCounter())))
+
+        const c = BabylonUtils.getCounter()
+        const newBullet = baby.baseBullet.clone("bullet" + c)
+        const bullet = new Bullet(obj.BULLET, newBullet)
+        baby.bullets.push(bullet)
+
+
+        const rX = newBullet.position.x + Math.sin(bullet.sBullet.point.rotation) * bullet.sBullet.speed * 200
+        const height = 0.25
+        const rZ = newBullet.position.z + Math.cos(bullet.sBullet.point.rotation) * bullet.sBullet.speed * 200
+        const path = [
+            new BABYLON.Vector3(newBullet.position.x, height, newBullet.position.z),
+            new BABYLON.Vector3(rX, height, rZ)
+        ]
+        const catmullRom = BABYLON.Curve3.CreateCatmullRomSpline(
+            path,
+            60
+        )
+
+        const animationPosition = new BABYLON.Animation("animPos" + c, "position", 30, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+        const keysPosition = []
+        for (let [p, item] of catmullRom.getPoints().entries()) {
+            keysPosition.push(
+                {
+                    frame: p,
+                    value: item
+                }
+            )
+        }
+
+        animationPosition.setKeys(keysPosition)
+        const animationGroup = new BABYLON.AnimationGroup("Group" + c)
+        animationGroup.addTargetedAnimation(animationPosition, newBullet)
+        animationGroup.play(true)
+
+        window.setTimeout(() => {
+            bullet.removeMe()
+        }, 3000)
     }
 
     constructor(obj, babylonObject) {
@@ -24,35 +55,6 @@ export class Bullet {
         this.bBullet.position.y = +obj.point.y
         this.bBullet.position.z = +obj.point.z
         this.bBullet.rotation.y = +obj.point.rotation + Math.PI
-
-        this.tick()
-        this.tick()
-        this.tick()
-
-        this.setupTicker()
-    }
-
-    tick() {
-
-        this.bBullet.position.x += Math.sin(this.sBullet.point.rotation) * this.sBullet.speed * 3
-        this.bBullet.position.z += Math.cos(this.sBullet.point.rotation) * this.sBullet.speed * 3
-
-        if (this.bBullet.position.x < 0) {
-            this.removeMe()
-            return
-        }
-        if (this.bBullet.position.z < 0) {
-            this.removeMe()
-            return
-        }
-        if (this.bBullet.position.x > 50 + 1) {
-            this.removeMe()
-            return
-        }
-        if (this.bBullet.position.z > 50 + 1) {
-            this.removeMe()
-            return
-        }
 
     }
 
